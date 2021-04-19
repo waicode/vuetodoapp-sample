d95ca8a6a3c8932b71c98e928aabd9a21a8f3bcc
waicode <waicode37@gmail.com>
2021-04-19 12:13:58 +0900

    feat: add sample data

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Hello World</h1>
+    <div>{{ text }}</div>
+    <div>{{ someNumber }}</div>
+    <div>{{ canDecide }}</div>
+    <div>{{ list }}</div>
+    <div>{{ someObject.a }}</div>
+    <div>{{ someObject.c }}</div>
+    <div>{{ dataFunction }}</div>
+    <div>{{ dataFunction() }}</div>
+    <div>{{ testFunctionA() }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import playground from "./playground";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      text: "Some String value",
+      someNumber: 16,
+      canDecide: true,
+      list: [1, 2, 3],
+      someObject: {
+        a: 1,
+        b: 2,
+        c: [1, 2, 3, 4, 5],
+      },
+      dataFunction() {
+        console.log("Data Function");
+        return "Data Function";
+      },
+    };
   },
   // this function is run automaticaly by Vue FW
   created() {
     playground();
+  },
+  methods: {
+    testFunctionA() {
+      console.log(this.text);
+      console.log(this.someNumber);
+      console.log(this.someObject.a);
+      this.dataFunction();
+      return "I am test Fn A";
+    },
   },
 };
 </script>
